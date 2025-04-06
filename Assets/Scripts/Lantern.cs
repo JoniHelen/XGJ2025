acd59ec81f2635c691eafc8162870fcf43c8475c
Joni Helén <91052459+JoniHelen@users.noreply.github.com>
2025-04-06 14:14:53 +0300

    lantern script

---
+++ Assets/Scripts/Lantern.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Lantern : MonoBehaviour {
+    [SerializeField] private float flickerSpeed;
+    [SerializeField] private Light2D lanternLight;
+
+    void Update() {
+        lanternLight.intensity = (Mathf.Sin(Time.time * flickerSpeed) + 1.0f) * 0.25f + 0.5f;
+    }
+}
